73ceb0dd4c558fe6c18777109318605fcf945d82
Laurel YAMA <ngwambilaj@gmail.com>
2024-10-08 14:52:24 +0100

    Votre message de commit ici

--- lib/pages/PhotoGallery.dart
+++ lib/pages/PhotoGallery.dart
@@ -8,77 +8,135 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('All Photo'),
-        ),
-        body: PhotoGalleryPage(),
+      home: DefaultTabController(
+        length: 2,
+        child: PhotoGalleryPage(),
       ),
     );
   }
 }
 
-class PhotoGalleryPage extends StatefulWidget {
+class PhotoGalleryPage extends StatelessWidget {
   @override
-  _PhotoGalleryPageState createState() => _PhotoGalleryPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.close), // Close icon
+          onPressed: () {
+            Navigator.pop(context); // Close the gallery
+          },
+        ),
+        title: Text('Photo Gallery'),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50.0), // Height of the tab bar
+          child: Row(
+            children: [
+              Expanded(
+                // Button "Official Photos"
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black, // Black background for official photos
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(0)), // Rounded corners
+                  ),
+                  child: Tab(
+                    child: Text(
+                      'Official Photos (14)',
+                      style: TextStyle(color: Colors.white), // White text
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                // Button "Custom Photos"
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white, // White background for custom photos
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(0),
+                        topRight: Radius.circular(10)), // Rounded corners
+                  ),
+                  child: Tab(
+                    child: Text(
+                      'Custom Photos (9617)',
+                      style: TextStyle(color: Colors.black), // Black text
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: TabBarView(
+        children: [
+          PhotoGrid(photoType: 'official', isOfficial: true),
+          PhotoGrid(photoType: 'custom', isOfficial: false),
+        ],
+      ),
+    );
+  }
 }
 
-class _PhotoGalleryPageState extends State<PhotoGalleryPage> {
-  bool showOfficialPhotos = true; // Boolean to switch between photo sets
+class PhotoGrid extends StatelessWidget {
+  final String photoType;
+  final bool isOfficial;
+
+  // Sample image URLs for official and custom photos
+  final List<String> officialPhotos = [
+    'https://th.bing.com/th/id/OIP.yj1iqhHtOV1R_IjSRh8OkwHaE7?w=626&h=417&rs=1&pid=ImgDetMain',
+    'https://th.bing.com/th/id/OIP.Mjo3a-YmNbc0DyFlSo8rIwHaE7?rs=1&pid=ImgDetMain',
+    'https://th.bing.com/th/id/OIP.uihM1HgOlSZs7y9XQ3-BnAHaJQ?rs=1&pid=ImgDetMain',
+    'https://th.bing.com/th/id/OIP.1lTc7YrLU_08ieoPDuNpBwHaFj?pid=ImgDet&w=185&h=138&c=7&dpr=1.3',
+    'https://th.bing.com/th/id/OIP.0xBwr74WxZZeI_nqBLopTgHaJQ?pid=ImgDet&w=185&h=231&c=7&dpr=1.3',
+    'https://th.bing.com/th/id/OIP.7KtcJy6rSWq1TBW4EHvyswHaFW?w=239&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+    'https://th.bing.com/th/id/OIP.cyS7ySJZZPmZaPJKUtUeKwHaE_?w=302&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+    'https://th.bing.com/th/id/OIP.fNvUY6OpY8Lp1EXCdvxABAHaKD?w=138&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+  ];
+
+  final List<String> customPhotos = [
+    'https://via.placeholder.com/150/FF5733/FFFFFF?text=Custom+Photo+1',
+    'https://via.placeholder.com/150/33FF57/FFFFFF?text=Custom+Photo+2',
+    'https://via.placeholder.com/150/3357FF/FFFFFF?text=Custom+Photo+3',
+    'https://via.placeholder.com/150/FF33A1/FFFFFF?text=Custom+Photo+4',
+    'https://via.placeholder.com/150/A133FF/FFFFFF?text=Custom+Photo+5',
+    'https://via.placeholder.com/150/33FFF5/FFFFFF?text=Custom+Photo+6',
+    'https://via.placeholder.com/150/F5FF33/FFFFFF?text=Custom+Photo+7',
+    'https://via.placeholder.com/150/FF3333/FFFFFF?text=Custom+Photo+8',
+  ];
+
+  PhotoGrid({required this.photoType, required this.isOfficial});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  showOfficialPhotos = true; // Show official photos
-                });
-              },
-              child: Text('Official Photos (14)'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: showOfficialPhotos
-                    ? Colors.blue
-                    : Colors.grey, // Use backgroundColor instead of primary
-              ),
-            ),
-            SizedBox(width: 10),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  showOfficialPhotos = false; // Show customer photos
-                });
-              },
-              child: Text('Customer Photos (9617)'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: !showOfficialPhotos
-                    ? Colors.blue
-                    : Colors.grey, // Use backgroundColor instead of primary
-              ),
-            ),
-          ],
-        ),
-        Expanded(
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 4.0,
-              mainAxisSpacing: 4.0,
-            ),
-            itemCount: showOfficialPhotos ? 14 : 9617,
-            itemBuilder: (context, index) {
-              return Image.network(
-                'https://th.bing.com/th/id/OIP.8Jp8Q8ICDfVSzWh98Q5u6gHaEO?rs=1&pid=ImgDetMain$index.jpg', // Replace with the actual image URLs
-                fit: BoxFit.cover,
-              );
+    // Select the appropriate photo list based on the type
+    List<String> photos = isOfficial ? officialPhotos : customPhotos;
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 4.0,
+        mainAxisSpacing: 4.0,
+      ),
+      itemCount: photos.length, // Use the length of the selected photo list
+      itemBuilder: (context, index) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(10), // Rounded corners for images
+          child: GestureDetector(
+            onTap: () {
+              // Action when tapping on the image
+              print(
+                  'Tapped on ${photoType} photo $index'); // Replace this with the desired action
             },
+            child: Image.network(
+              photos[index], // Use the selected photo URL
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-      ],
+        );
+      },
     );
   }
 }
--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,114 +8,166 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Travel Booking App'),
-        ),
-        body: TravelBookingPage(),
+        // Suppression de l'AppBar pour mettre l'image directement en haut
+        body: DestinationCard(),
       ),
     );
   }
 }
 
-class TravelBookingPage extends StatelessWidget {
+class DestinationCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.network('URL_DE_VOTRE_IMAGE'),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Explore Thailand\'s Impressive Wooden Religious Shrine And Monument',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
+          // Image en haut sans marge ni espace
+          Image.network(
+            'https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_862/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/riwuojas4vdmpyr2biqu/TheSanctuaryofTruthTicketinPattaya.jpg',
+            height: 250,
+            width: double.infinity,
+            fit: BoxFit.cover, // L'image couvre tout l'espace disponible
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Row(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                RatingBar.builder(
-                  initialRating: 4.5,
-                  minRating: 1,
-                  direction: Axis.horizontal,
-                  allowHalfRating: true,
-                  itemCount: 5,
-                  itemBuilder: (context, _) => Icon(
-                    Icons.star,
-                    color: Colors.amber,
+                Text(
+                  'The Sanctuary Of Truth',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
                   ),
-                  onRatingUpdate: (rating) {
-                    print(rating);
-                  },
                 ),
-                SizedBox(width: 10),
-                Text('4.5 (60,625 reviews)'),
+                SizedBox(height: 10),
+                Text(
+                  'Explore Thailand\'s Impressive Wooden Religious Shrine And Monument',
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Icon(Icons.star, color: Colors.orange),
+                    SizedBox(width: 5),
+                    Text('4.5 (4,625 Reviews)', style: TextStyle(fontSize: 14)),
+                    SizedBox(width: 5),
+                    Text('60K+ Booked',
+                        style: TextStyle(fontSize: 14, color: Colors.grey)),
+                  ],
+                ),
+                SizedBox(height: 10),
+                // Section avec les icônes noires
+                DetailRow(
+                    icon: Icons.today,
+                    text: 'Available Today',
+                    iconColor: Colors.black),
+                DetailRow(
+                    icon: Icons.flash_on,
+                    text: 'Instant Confirmation',
+                    iconColor: Colors.black),
+                DetailRow(
+                    icon: Icons.cancel,
+                    text: 'No Cancellation/Free Cancellation - 24 Hours Notice',
+                    iconColor: Colors.black),
+                DetailRow(
+                    icon: Icons.phone_android,
+                    text: 'Show Mobile Or Printed Voucher',
+                    iconColor: Colors.black),
+                DetailRow(
+                    icon: Icons.date_range,
+                    text: 'Open Date Ticket/Fixed Date Ticket',
+                    iconColor: Colors.black),
+                DetailRow(
+                    icon: Icons.store,
+                    text: 'Collect Physical Ticket',
+                    iconColor: Colors.black),
+                DetailRow(
+                    icon: Icons.location_on,
+                    text: 'Meet Up At Location',
+                    iconColor: Colors.black),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      '₹ 942',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      '₹ 1,223',
+                      style: TextStyle(
+                        fontSize: 16,
+                        decoration: TextDecoration.lineThrough,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      '23% OFF',
+                      style: TextStyle(fontSize: 16, color: Colors.green),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text('ADD TO CART'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          padding: EdgeInsets.symmetric(vertical: 15),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text('BOOK NOW'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green,
+                          padding: EdgeInsets.symmetric(vertical: 15),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Icon(Icons.check_circle, color: Colors.green),
-                SizedBox(width: 5),
-                Text('Instant Confirmation'),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Icon(Icons.cancel, color: Colors.red),
-                SizedBox(width: 5),
-                Text('Free Cancellation within 24 hours'),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Icon(Icons.calendar_today, color: Colors.blue),
-                SizedBox(width: 5),
-                Text('Open Date Ticket / Fixed Date Ticket'),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Price: 942 THB',
-              style: TextStyle(
-                fontSize: 18,
-                decoration: TextDecoration.lineThrough,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Discounted Price: 723 THB (23% off)',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.green,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Action to add to cart
-              },
-              child: Text('Add to Cart'),
-            ),
-          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DetailRow extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color iconColor;
+
+  DetailRow(
+      {required this.icon, required this.text, this.iconColor = Colors.black});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        children: [
+          Icon(icon, color: iconColor, size: 20),
+          SizedBox(width: 10),
+          Expanded(child: Text(text)),
         ],
       ),
     );
